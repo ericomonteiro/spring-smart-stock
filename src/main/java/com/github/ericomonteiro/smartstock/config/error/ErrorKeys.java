5fece17676b94e644822bee5984750ea2809b799
Erico Monteiro <erico.cintra@gmail.com>
2021-03-30 12:24:09 -0300

    created exception handler

---
+++ src/main/java/com/github/ericomonteiro/smartstock/config/error/ErrorKeys.java
@@ -0,0 +1,12 @@
+package com.github.ericomonteiro.smartstock.config.error;
+
+public class ErrorKeys {
+
+    public static class Product {
+        public static final String NAME_NOT_BLANK = "product-1";
+        public static final String DETAIL_NOT_BLANK = "product-2";
+        public static final String PRICE_MUST_GREATER_ZERO = "product-3";
+        public static final String PRICE_NOT_NULL = "product-4";
+    }
+
+}
--- src/main/java/com/github/ericomonteiro/smartstock/model/dto/product/ProductCreateDto.java
+++ src/main/java/com/github/ericomonteiro/smartstock/model/dto/product/ProductCreateDto.java
@@ -2,19 +2,25 @@ package com.github.ericomonteiro.smartstock.model.dto.product;
 
 import lombok.Data;
 import lombok.ToString;
-
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+
+import static com.github.ericomonteiro.smartstock.config.error.ErrorKeys.Product.NAME_NOT_BLANK;
+import static com.github.ericomonteiro.smartstock.config.error.ErrorKeys.Product.DETAIL_NOT_BLANK;
+import static com.github.ericomonteiro.smartstock.config.error.ErrorKeys.Product.PRICE_MUST_GREATER_ZERO;
+import static com.github.ericomonteiro.smartstock.config.error.ErrorKeys.Product.PRICE_NOT_NULL;
 
 @Data
 @ToString
 public class ProductCreateDto {
-    @NotBlank
+    @NotBlank(message = NAME_NOT_BLANK)
     private String name;
 
-    @NotBlank
+    @NotBlank(message = DETAIL_NOT_BLANK)
     private String details;
 
-    @Positive
+    @Positive(message = PRICE_MUST_GREATER_ZERO)
+    @NotNull(message = PRICE_NOT_NULL)
     private Float price;
 }
