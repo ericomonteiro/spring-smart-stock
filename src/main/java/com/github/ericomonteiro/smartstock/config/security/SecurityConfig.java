06f87d42a3f8363db846e6450c12c84ce4ab6d95
Erico Monteiro <erico.cintra@gmail.com>
2021-03-31 18:45:17 -0300

    organize imports

--- src/main/java/com/github/ericomonteiro/smartstock/config/security/SecurityConfig.java
+++ src/main/java/com/github/ericomonteiro/smartstock/config/security/SecurityConfig.java
@@ -1,15 +1,12 @@
 package com.github.ericomonteiro.smartstock.config.security;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.core.convert.converter.Converter;
 
